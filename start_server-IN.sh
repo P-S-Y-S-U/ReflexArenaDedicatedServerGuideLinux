9cb954e2c0ef546070e18e3ba480412c8ef504b5
P-S-Y-S-U <mohan.gaming78@gmail.com>
2025-08-26 15:59:20 +0530

    Update start_server-IN.sh

--- start_server-IN.sh
+++ start_server-IN.sh
@@ -1,2 +1,2 @@
-screen -dmS rickflex-sea-screen
-screen -S rickflex-sea-screen -X stuff "cd /home/rick/reflex/ && xvfb-run -a wine /home/rick/reflex/reflexded.exe +loadconfig rickflex-server-SEA\n"
+screen -dmS rickflex-in-screen
+screen -S rickflex-in-screen -X stuff "cd /home/rick/reflex/ && xvfb-run -a wine /home/rick/reflex/reflexded.exe +loadconfig rickflex-server-SEA\n"
